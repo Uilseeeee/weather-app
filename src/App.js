ff0bfc74d3c93af6648c07907345740442037629
Uilsee <uilseeu437@gmail.com>
2025-01-15 09:58:19 +0800

    trying tailwind

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 import { useEffect, useState } from 'react';
+
+
 function App() {
   const [countriesSearch, setCountriesSearch] = useState("");
   const [countriesData, setCountriesData] = useState([]);
@@ -9,17 +11,17 @@ function App() {
   // api data insert //
   const fetchData = () => {
     fetch("https://countriesnow.space/api/v0.1/countries")
-    .then((response) =>response.json()
-    ).then((result) => {
-      setCountriesData(result.data)
-      setFilteredData(result.data)
-    }).catch((error) => {
-      console.log("Error", error);
+      .then((response) => response.json()
+      ).then((result) => {
+        setCountriesData(result.data)
+        setFilteredData(result.data)
+      }).catch((error) => {
+        console.log("Error", error);
 
-    });
-    console.log("owww");
-    
-    
+      });
+
+
+
   };
   const filterData = () => {
     setFilteredData(
@@ -33,7 +35,7 @@ function App() {
           .split("")
           .includes(countriesSearch.toUpperCase())
         )
-        return item ;
+          return item;
       })
     );
   }
@@ -59,28 +61,32 @@ function App() {
 
 
   return (
-
-    <div>
-
+ 
+    <div className='bg-black flex justify-center items-center h-[1200px] w-[1600px]'>
+     <div className="rounded-full bg-white h-[140px] w-[140px]"></div>
+      <img className='h-[100px] w-[100px]'
+        src='sun.png'
+        alt=''
+      />
       <div>
         <div>
-          <input onChange={handleChange} placeholder='Search country' className='searchInput' />
+          <input onChange={handleChange} placeholder='Search country' className='h-[50px] w-[300px]' />
         </div>
         <div>
           {filteredData.map((country, index) => {
-            return <div key={index}>{country.country}</div>;
+            return <div className="" key={index}>{country.country}</div>;
           })}
         </div>
       </div>
 
 
-      <div className='boardPosition'>
-        <div className='whiteScreen'>
-          <div className='whiteScreenInfo'>
+      <div className="">
+        <div className="">
+          <div className="">
           </div>
         </div>
-        <div className='blackScreen'>
-          <div className='blackScreenInfo'></div>
+        <div className="">
+          <div className=""></div>
         </div>
       </div>
     </div>
